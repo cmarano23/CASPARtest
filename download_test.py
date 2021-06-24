a4190f9468f5647e951b048060cd52aedd7ee905
Carson Marano <cmarano23@amherst.edu>
2021-06-24 17:01:24 -0400

    updates

--- download_test.py
+++ download_test.py
@@ -4,13 +4,14 @@ import datetime
 import csv
 
 #Download spreadsheet
-url = 'https://docs.google.com/spreadsheets/d/1X8jb3bc6dVpKDpUkt1XA2x3TWtJaLuDWC2qByocMiFg/export?gid=871681305&format=csv'
-urllib.request.urlretrieve(url, '/Users/carsonmarano/Desktop/SURF2021/Code/CASPARtest/testCASPAR.csv')
+url_csv = 'https://docs.google.com/spreadsheets/d/1X8jb3bc6dVpKDpUkt1XA2x3TWtJaLuDWC2qByocMiFg/export?format=csv'
+url_xlsx = 'https://docs.google.com/spreadsheets/d/1X8jb3bc6dVpKDpUkt1XA2x3TWtJaLuDWC2qByocMiFg/export?format=xlsx'
+urllib.request.urlretrieve(url_csv, '/Users/carsonmarano/Desktop/SURF2021/Code/CASPARtest/testCASPAR.csv')
+urllib.request.urlretrieve(url_xlsx, '/Users/carsonmarano/Desktop/SURF2021/Code/CASPARtest/testCASPAR.xlsx')
 
 #Function to add, commit, and push to git
 def uploadToGit():
 	message = "Automated update " + str(datetime.datetime.now())
-	#os.system("cd ~/Desktop/SURF2021/Code/CASPARtest")
 	os.system("git fetch")
 	os.system("git add " + "testCASPAR.csv")
 	os.system("git commit -m " + "'" + message +"'")
